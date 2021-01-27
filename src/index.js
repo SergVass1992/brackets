e2b26107f4ad891759511c916d12a40e9f313408
Сергей <56017574+SERgey1312@users.noreply.github.com>
2021-01-27 21:42:27 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets_array = bracketsConfig.map((el) => el.join('')); 
+  for(let i = 0; i < brackets_array.length; i++){
+  	if (str.includes(brackets_array[i])) {
+  		str = str.replace(brackets_array[i], '');
+  		i = -1;
+  	}
+  }
+  return str == '' ? true : false;
 }
